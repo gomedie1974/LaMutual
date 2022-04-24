4c0a0c044bbdeed5e81d2cf260816e984cc64c2d
Diego Gomez <gomedie1974@hotmail.com>
2022-04-24 12:10:07 -0300

    segundo proyecto

---
+++ app.js
@@ -0,0 +1,18 @@
+/* let rayban = document.getElementById("info");
+rayban.addEventListener("click", mensaje);
+
+function mensaje(){
+    Swal.fire({
+        text: 'Puede solicitar los catálogos de anteojos de las 3 marcas (RayBan/Ferrari/Oakley) por mail y le sera enviado a la brevedad posible, para luego poder realizar el pedido correspondiente.',
+      })    
+};
+ *//* 
+let rapsodia = document.getElementById("infoR");
+rapsodia.addEventListener("click", mensajeR);
+
+function mensajeR(){
+    Swal.fire({
+        text: 'Toodos los locales habilitados en Argentina, excepto LOCALES VINTAGE.',
+      })    
+};
+ */
